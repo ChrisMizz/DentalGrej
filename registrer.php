ceaa793cb9b27d45fdd37389dc5477797e290266
ChrisMizz <Christofferdetlef@gmail.com>
2017-03-29 11:04:50 +0200

    Mindst valgt en pakke for submit

--- registrer.php
+++ registrer.php
@@ -48,69 +48,73 @@
     <div>Beløb: <span id="total-cart"></span>DKK</div>
 </div>
     <script>
-		$(".add-to-cart").click(function(event){
-				event.preventDefault();
-				var name = $(this).attr("data-name");
-				var price = Number($(this).attr("data-price"));
+$(".add-to-cart").click(function(event){
+		event.preventDefault();
+		var name = $(this).attr("data-name");
+		var price = Number($(this).attr("data-price"));
 
-				shoppingCart.addItemToCart(name, price, 1);
-				displayCart();
-		});
-
-		$("#clear-cart").click(function(event){
-				shoppingCart.clearCart();
-				displayCart();
-		});
-
-		function displayCart() {
-				var cartArray = shoppingCart.listCart();
-				console.log(cartArray);
-				var output = "";
-
-				for (var i in cartArray) {
-						output += "<li>"
-								+cartArray[i].name
-								+" <input class='item-count' data-name='"
-								+cartArray[i].name
-								+"' value='"+cartArray[i].count+"' >"
-								+" x "+cartArray[i].price
-								+" = "+cartArray[i].total
-								+"</li>";
-				}
-
-				$("#show-cart").html(output);
-				$("#count-cart").html( shoppingCart.countCart() );
-				$("#total-cart").html( shoppingCart.totalCart() );
-		}
-
-		$("#show-cart").on("click", ".delete-item", function(event){
-				var name = $(this).attr("data-name");
-				shoppingCart.removeItemFromCartAll(name);
-				displayCart();
-		});
-
-		$("#show-cart").on("click", ".subtract-item", function(event){
-				var name = $(this).attr("data-name");
-				shoppingCart.removeItemFromCart(name);
-				displayCart();
-		});
-
-		$("#show-cart").on("click", ".plus-item", function(event){
-				var name = $(this).attr("data-name");
-				shoppingCart.addItemToCart(name, 0, 1);
-				displayCart();
-		});
-
-		$("#show-cart").on("change", ".item-count", function(event){
-				var name = $(this).attr("data-name");
-				var count = Number($(this).val());
-				shoppingCart.setCountForItem(name, count);
-				displayCart();
-		});
-
-
+		shoppingCart.addItemToCart(name, price, 1);
 		displayCart();
+});
 
+$("#clear-cart").click(function(event){
+		shoppingCart.clearCart();
+		displayCart();
+});
+
+function displayCart() {
+		var cartArray = shoppingCart.listCart();
+		console.log(cartArray);
+		var output = "";
+			for (var i in cartArray) {
+				output += "<li>"
+					+cartArray[i].name
+                    +" <input disabled class='item-count' data-name='"
+                    +cartArray[i].name
+                    +"' value='"+cartArray[i].count+"' >"
+					+"</li>";
+			}
+		$("#show-cart").html(output);
+		$("#count-cart").html( shoppingCart.countCart() );
+		$("#total-cart").html( shoppingCart.totalCart() );
+}
+
+$("#show-cart").on("click", ".delete-item", function(event){
+		var name = $(this).attr("data-name");
+		shoppingCart.removeItemFromCartAll(name);
+		displayCart();
+});
+
+$("#show-cart").on("click", ".subtract-item", function(event){
+		var name = $(this).attr("data-name");
+		shoppingCart.removeItemFromCart(name);
+		displayCart();
+});
+
+$("#show-cart").on("click", ".plus-item", function(event){
+		var name = $(this).attr("data-name");
+		shoppingCart.addItemToCart(name, 0, 1);
+		displayCart();
+});
+
+$("#show-cart").on("change", ".item-count", function(event){
+		var name = $(this).attr("data-name");
+		var count = Number($(this).val());
+		shoppingCart.setCountForItem(name, count);
+		displayCart();
+});
+
+displayCart();
+
+$("#formSubmit").submit(function(){
+	if ($('.item-count').val() >= 1) {
+		return true;
+	}
+	else {
+		alert('Du skal vælge mindst en pakke')
+		return false;
+		}
+})
 </script>
        <fieldset>
             <legend>Opret profil</legend>
