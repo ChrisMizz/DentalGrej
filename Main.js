bd1cb8a895bc11a6b10d339ba94d1c5716b1b91a
ChrisMizz <Christofferdetlef@gmail.com>
2017-04-02 21:27:12 +0200

    Rettet en fejl(ups)

--- Main.js
+++ Main.js
@@ -20,8 +20,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    } 
-    else {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
     }
 }
 
